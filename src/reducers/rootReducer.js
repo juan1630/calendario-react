020e427224d45acd000968b06d9879ad3b444dfe
Juan Patrón <josejuanpatron1630@gmail.com>
2021-10-27 23:44:18 -0500

    Se agrego el dispatch del login

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,13 +1,14 @@
 // el root reducer es la combinacion de todos los reducers 
 
 import { combineReducers } from 'redux'
+import { authReducer } from './authReducer'
 import { calendarReducer } from './calendarReducer'
 import { uiReducer } from './uiReducer'
 
 export const rootReducer = combineReducers({
     ui:uiReducer,
-    calendar: calendarReducer
-    //TODO: EL AUTH REDUCER FALTA
+    calendar: calendarReducer,
+    auth: authReducer
 })
 
 // ES COMO VA A LUCIR EL STORE
--- src/types/types.js
+++ src/types/types.js
@@ -9,7 +9,14 @@ export const types = {
     eventAddNew: '[event] Add New',
     eventClearActiveNote: '[event] Clear active note',
     eventUpdated: '[event] Event updated',
-    eventDeleted : "[event] Event deleted"
+    eventDeleted : "[event] Event deleted",
 
+    authCheking: '[auth] Checking login state',
+    authChekingFinish: '[auth] Finish  cheking login state',
+    authStartLogin: '[auth] Start login',
+    authLogin: '[auth] login',
+    authStartRegister: '[auth] Start Register',
+    authStartTokenReNew: '[auth] Start token renew',
+    authLogout: '[auth] Logout'
 }
 
