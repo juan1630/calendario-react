e9f6763fe20c5cc057b8bb9debb6d9d623348ca2
Juan Patrón <josejuanpatron1630@gmail.com>
2021-12-20 23:49:35 -0600

    Eliminacion de los eventos

--- src/components/ui/EventDeleteFab.js
+++ src/components/ui/EventDeleteFab.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
-import { eventDeleted } from '../../actions/events';
+import {  startDeleteEvent } from '../../actions/events';
 
 export const EventDeleteFab = () => {
 
     const dispatch = useDispatch();
 
     const handleDeleteEvent = () => {
-        dispatch( eventDeleted())
+        
+        dispatch( startDeleteEvent())
+
     }
 
     return (
